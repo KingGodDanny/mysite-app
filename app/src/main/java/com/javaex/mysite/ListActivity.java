a79edf2cb8e63e12081685ee42fb74fcb447be24
Danny <ctct25@naver.com>
2021-08-22 16:42:11 +0900

    1st Android commit

--- app/src/main/java/com/javaex/mysite/ListActivity.java
+++ app/src/main/java/com/javaex/mysite/ListActivity.java
@@ -2,6 +2,7 @@ package com.javaex.mysite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,8 +10,16 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.javaex.vo.GuestbookVo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,19 +33,19 @@ public class ListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list);
 
         //데이터를 가져온다.(서버로부터) -- 가상
-        List<GuestbookVo> guestbookList = getListFromServer();
-        Log.d("javaStudy", "서버로부터 데이터 수신........옼케이!");
-        Log.d("javaStudy", "list:" + guestbookList.toString());
+        //<GuestbookVo> guestbookList = getListFromServer();
+
 
         //리스트뷰를 객체화 한다.
         lvGuestbookList = (ListView) findViewById(R.id.lvGuestbookList);
 
-        //어댑터를 생성
-        GuestbookListAdapter guestbookListAdapter =
-                new GuestbookListAdapter(getApplicationContext(), R.id.lvGuestbookList, guestbookList);
 
-        //리스트뷰에 어댑터를 세팅한다.
-        lvGuestbookList.setAdapter(guestbookListAdapter);
+        //나가서 일해라 -->데이터 가져오기 --> 화면에 그리기(어댑터)
+        ListAsyncTask listAsyncTask = new ListAsyncTask();
+        listAsyncTask.execute();
+
+
+
 
         //해당 방명록의 컨텐츠를 읽기위한 클릭이벤트
         lvGuestbookList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -87,6 +96,114 @@ public class ListActivity extends AppCompatActivity {
       return guestbookList;
 
     }
+
+
+    //이너클래스
+    public class ListAsyncTask extends AsyncTask< Void, Integer, List<GuestbookVo>> {
+        //제일 처음
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+
+        @Override
+        protected List<GuestbookVo> doInBackground(Void... voids) {
+
+            //리턴값으로 보내기 때문에 null로 만든다.
+            List<GuestbookVo> guestbookList = null;
+
+            //서버에 연결 한다.
+            //요청을 한다.
+            try {
+                URL url = new URL("http://192.168.55.74:8088/mysite5/api/guestbook/list");  //url 생성 -> 없는주소 치면 resCode는 에러코드랑 숫자 똑같이 뜸.
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();  //url 연결
+                conn.setConnectTimeout(10000); // 10초 동안 기다린 후 응답이 없으면 종료
+                conn.setRequestMethod("POST"); // 요청방식 POST
+                conn.setRequestProperty("Content-Type", "application/json"); //요청시 데이터 형식 json
+                conn.setRequestProperty("Accept", "application/json"); //응답시 데이터 형식 json
+                conn.setDoOutput(true); //OutputStream으로 POST 데이터를 넘겨주겠다는 옵션.
+                conn.setDoInput(true); //InputStream으로 서버로 부터 응답을 받겠다는 옵션.
+
+                int resCode = conn.getResponseCode(); // 응답코드 200이 정상
+                Log.d("javaStudy", "resCode = " + resCode);
+
+                if (resCode == 200) { //정상이면
+
+                    //Stream 을 통해 통신한다
+                    //데이타 형식은 json으로 한다.
+                    InputStream is = conn.getInputStream();
+                    //문자열로 변환
+                    InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+                    //읽어야 됨
+                    BufferedReader br = new BufferedReader(isr);
+
+
+                    //읽어오셈
+                    //한줄씩 읽어옴 -> 반복해서 읽어와야되니까 반복문 ㄱ
+                    //라인이 null이면 끝내셈
+                    String jsonData = "";
+                    while (true) {
+
+                        String line = br.readLine();
+
+                        if (line == null) {
+                            break;
+                        }
+
+                        jsonData = jsonData + line;
+                        //jsonData += line;
+                    }
+
+                    //json --> 자바객체로 만든다.
+                    //Log.d("javaStudy", "jsonData = " + jsonData);
+                    Gson gson = new Gson();
+                    guestbookList = gson.fromJson(jsonData, new TypeToken<List<GuestbookVo>>() {
+                    }.getType());
+
+
+                    //Log.d("javaStudy", "size = " + guestbookList.size());
+                    //Log.d("javaStudy", "index(0).name = " + guestbookList.get(0).getName());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            //응답을 받는다.(데이터를 받는다(문자열)) -> guestbooklist 받아야함 -> json형태로 온다 -> java객체로 변환(List<GuestbookVo> guestbookVo)
+            return guestbookList;
+
+        }
+
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+        }
+
+
+        @Override
+        protected void onPostExecute(List<GuestbookVo> guestbookList) {
+            Log.d("javaStudy", "onPostExecute");
+            Log.d("javaStudy", "size = " + guestbookList.size());
+            Log.d("javaStudy", "index(0).name = " + guestbookList.get(0).getName());
+
+            //어댑터를 생성   -- 데이터를 가져와야 어댑터생성이가능
+            GuestbookListAdapter guestbookListAdapter =
+                    new GuestbookListAdapter(getApplicationContext(), R.id.lvGuestbookList, guestbookList);
+
+
+            //리스트뷰에 어댑터를 세팅한다.
+            lvGuestbookList.setAdapter(guestbookListAdapter);
+
+
+            super.onPostExecute(guestbookList);
+        }
+
+    }
+
+
 
 
 
--- app/src/main/java/com/javaex/mysite/MainActivity.java
+++ app/src/main/java/com/javaex/mysite/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -12,7 +13,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.javaex.vo.GuestbookVo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -58,12 +72,13 @@ public class MainActivity extends AppCompatActivity {
                 GuestbookVo guestbookVo = new GuestbookVo(name, password, content);
                 Log.d("javaStudy", "vo= " + guestbookVo.toString());
 
+
+                WriteAsyncTask writeAsyncTask = new WriteAsyncTask();
+                writeAsyncTask.execute(guestbookVo);
+
+
                 //서버에 전송하고
                 Log.d("javaStudy", "서버전송.............Ok!!!");
-
-                //리스트 액티비티로 전환
-                Intent intent = new Intent(MainActivity.this, ListActivity.class);
-                startActivity(intent);
 
 
 
@@ -89,6 +104,95 @@ public class MainActivity extends AppCompatActivity {
 
 
         }
+
+
+    }
+
+
+    //이너클래스
+    public class WriteAsyncTask extends AsyncTask<GuestbookVo, Integer, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+
+        @Override
+        protected String doInBackground(GuestbookVo... guestbookVos) {
+
+            Log.d("javaStudy", "doInBackground()");
+            Log.d("javaStudy", "Vo = " + guestbookVos[0].toString());
+
+            //vo-->json
+            Gson gson = new Gson();
+            String json = gson.toJson(guestbookVos[0]);
+            Log.d("javaStudy", "json -->" + json);
+
+            //데이터 전송
+
+
+            try {
+                //접속정보
+                URL url = new URL("http://192.168.55.74:8088/mysite5/api/guestbook/write2");  //url 생성 -> 없는주소 치면 resCode는 에러코드랑 숫자 똑같이 뜸.
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();  //url 연결
+                conn.setConnectTimeout(10000); // 10초 동안 기다린 후 응답이 없으면 종료
+                conn.setRequestMethod("POST"); // 요청방식 POST
+                conn.setRequestProperty("Content-Type", "application/json"); //요청시 데이터 형식 json
+                conn.setRequestProperty("Accept", "application/json"); //응답시 데이터 형식 json
+                conn.setDoOutput(true); //OutputStream으로 POST 데이터를 넘겨주겠다는 옵션.
+                conn.setDoInput(true); //InputStream으로 서버로 부터 응답을 받겠다는 옵션.
+
+                //outputStream(json ---> body)
+                OutputStream os = conn.getOutputStream();
+                OutputStreamWriter osw = new OutputStreamWriter(os);
+                BufferedWriter bw = new BufferedWriter(osw);
+
+                bw.write(json);
+                bw.flush();
+
+                int resCode = conn.getResponseCode(); // 응답코드 200이 정상
+                Log.d("javaStudy", "resCode = " + resCode);
+
+                if(resCode == HttpURLConnection.HTTP_OK) {  //http_ok 는 숫자로 200이고 그거와같다면.
+
+
+                    //리스트 액티비티로 전환
+                    Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                    startActivity(intent);
+
+
+                }
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            //응답을 받는다.(데이터를 받는다(문자열)) -> guestbooklist 받아야함 -> json형태로 온다 -> java객체로 변환(List<GuestbookVo> guestbookVo)
+            return null;
+
+
+
+
+
+
+        }
+
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+        }
+
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+
 
 
     }
